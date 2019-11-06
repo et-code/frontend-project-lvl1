09838fac5ef038721be3f4d7bb63e0fe09c649e4
Evgeniy Trifonov <4etrifonov@gmail.com>
2019-11-06 23:01:18 +0300

    CodeClimate, Eslint, Travis-ci

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
-import { greeting } from '..';
+import greeting from '..';
+
+greeting();
--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
-greeting();
 
-export { greeting };
+export default { greeting };
