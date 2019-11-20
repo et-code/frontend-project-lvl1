18e515a17f71d08c17b04a6b6c6b0a237d4d7f20
Evgeniy Trifonov <4etrifonov@gmail.com>
2019-11-20 21:17:10 +0300

    refactoring

--- src/index-brain-games.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import readlineSync from 'readline-sync';
-
-export default () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  return userName;
-};
