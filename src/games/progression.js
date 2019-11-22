17c8215593375813c93fd4a4ee625456918b76b5
Evgeniy Trifonov <4etrifonov@gmail.com>
2019-11-23 01:32:38 +0300

    refactoring brain-progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,13 +4,13 @@ import getRandomNum from '../number-generator';
 
 const description = 'What number is missing in the progression?';
 const generateArrayOfNumbers = () => {
-  let randomNum = getRandomNum(10, 1);
+  const randomNum = getRandomNum(10, 1);
   const step = getRandomNum(5, 1);
   const arrayOfNumbers = [];
   const lengthOfArray = 10;
   for (let i = 0; i < lengthOfArray; i += 1) {
-    arrayOfNumbers.push(randomNum);
-    randomNum += step;
+    const nextNum = randomNum + i * step;
+    arrayOfNumbers.push(nextNum);
   }
   const indexOfArray = getRandomNum(8, 1);
   const answer = `${arrayOfNumbers.splice(indexOfArray, 1, '..')}`;
